884bddd06d71ffa2e7bc4c2ec483f14daf5838a2
Abrar Ajaz Wani <abhaywani114@gmail.com>
2025-12-25 00:12:43 +0530

    day 2

---
+++ WorkingWithArrays/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+    int cols, rows;
+
+    Console.Write("How many rows? ");
+    rows = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("How many cols? ");
+    cols = Convert.ToInt32(Console.ReadLine());
+
+    int[][] matrix = new int[rows][];
+
+    Console.WriteLine("Initilizing array...");
+    for (int i = 0; i < matrix.Length; i++)
+        matrix[i] = new int[cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            Console.Write($"Enter value for matrix[{i}][{j}]:\t");
+            matrix[i][j] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    Console.WriteLine("Printing your matrix: ");
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            Console.Write($"{matrix[i][j]}\t");
+            Thread.Sleep(500 * i);
+            Console.Beep();
+        }
+        Console.WriteLine();
+    }
+}
+catch (Exception ex)
+{
+    // just ignore
+}
